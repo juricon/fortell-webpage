63f537d633c9cebe1571d69002110389f957a900
juricon <yuriyvladim@gmail.com>
2016-08-13 02:23:35 +0300

    almost done  3hw

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function(){
-    $('.third-block-bxslider').bxSlider();
+    $('.third-block-bxslider').bxSlider({
+        pager:false
+    });
+    $('.seventh-block-slider-squares').bxSlider({
+        auto: true,
+        controls:false,
+        pause: 5000
+    });
 });
+
+/*
+$main_color_light:rgb(122, 122, 122);
+$main_color_dark:rgb(96, 96, 96);
+$main_color_active:rgb(26, 187, 156);
+$main_transition:background-color 200ms ease-out 0.1s;
+*/
